c46714e15767d12b83f02803ac8a2b56140d5835
Aluko Opeyemi F <folajimiopeyemisax13@gmail.com>
2022-06-14 08:03:43 +0100

    ...

--- client/src/Components/Dashboard/Sidebar/Sidebar.jsx
+++ client/src/Components/Dashboard/Sidebar/Sidebar.jsx
@@ -23,29 +23,29 @@ export const Sidebar = () => {
           <h6 className="text-muted mb-4">AGENCY-TRAVEL</h6>
           <div className="ms-3">
             <h5>
-              <a href="/" className="text-decoration-none text-white">
+              <a href="#" className="text-decoration-none text-white">
                 <JournalCheck className="me-2" /> APPLICATIONS
               </a>
             </h5>
             <h5>
-              <a href="/" className="text-decoration-none text-white">
+              <a href="#" className="text-decoration-none text-white">
                 <Speedometer className="me-2" /> DASHBOARD
               </a>
             </h5>
             <h5>
-              <a href="/" className="text-decoration-none text-white">
+              <a href="#" className="text-decoration-none text-white">
                 <Cash className="me-2" /> PAYMENT REPORT
               </a>
             </h5>
             <h5>
-              <a href="/" className="text-decoration-none text-white">
+              <a href="#" className="text-decoration-none text-white">
                 <Person className="me-2" /> USER MANAGEMENT
               </a>
             </h5>
           </div>
           <div className="logout ms-3">
             <h5 className="text-danger d-flex align-items-center">
-              <a href="/" className="text-decoration-none text-danger">
+              <a href="#" className="text-decoration-none text-danger">
                 <BoxArrowLeft className="me-2" /> LOGOUT
               </a>
             </h5>
